463c010713f48aabd8d516c390781538eff8b957
Daniel L <daniel.llupercio@gmail.com>
2023-01-22 14:55:59 -0500

    Add project files.

---
+++ Heads_First_Design/Chapter_1/SimUDuck/SimUDuck/Duck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimUDuck
+{
+    public abstract class Duck
+    {
+        FlyBehavior flyBehavior;
+        QuackBehavior quackBehavior;
+        public Duck()
+        { }
+    }
+
+}
---
+++ Heads_First_Design/Chapter_1/SimUDuck/SimUDuck/MallardDuckcs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimUDuck
+{
+    public class MallardDuck : Duck
+    {
+        public MallardDuck()
+        {
+            quackBehavior = new Quack();
+            flyBehavior = new FlyWithWings();
+        }
+
+        public void display()
+        {
+            Console.WriteLine("I'm a real Mallard duck");
+        }
+    }
+}
