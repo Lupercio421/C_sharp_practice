6d40424a0c3bd212f24711de643e7dec895ebfba
Daniel L <daniel.llupercio@gmail.com>
2023-01-22 21:18:42 -0500

    changing the username of a single item

---
+++ Cosmos_DB_Bulk/SinglePatch.cs
@@ -0,0 +1,31 @@
+﻿using ItemDetail;
+using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmos_DB_Bulk
+{
+    public class SinglePatch
+    {
+
+        public static async Task PatchSingleJson()
+        {
+            CosmosClient cosmosClient = new CosmosClient(Program.EndpointUrl, Program.AuthorizationKey, new CosmosClientOptions() { AllowBulkExecution = true });
+            Database database = cosmosClient.GetDatabase(Program.DatabaseName);
+            Container container = database.GetContainer(Program.ContainerName);
+
+            Console.WriteLine("\n1.6 - Patching a item using its Id");
+
+            ItemResponse<Item> response = await container.PatchItemAsync<Item>(
+                id: "7864afb5-14a0-4549-a8eb-dcc40e06b3b7",
+                partitionKey: new PartitionKey("7864afb5-14a0-4549-a8eb-dcc40e06b3b7"),
+                patchOperations: new[] { PatchOperation.Replace("/username", "Danny") });
+
+            Item updatedItem = response.Resource;
+            Console.WriteLine($"Username of updated item: {updatedItem.username}");
+        }
+    }
+}
