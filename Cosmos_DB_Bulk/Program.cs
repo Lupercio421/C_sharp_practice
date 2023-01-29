fd8147c220576715f2865815e3c58f925234bb7e
Daniel L <daniel.llupercio@gmail.com>
2023-01-29 16:07:14 -0500

    removing keys

--- Cosmos_DB_Bulk/Program.cs
+++ Cosmos_DB_Bulk/Program.cs
@@ -9,6 +9,8 @@ using Cosmos_DB_Bulk;
 using CosmosDBBulk;
 using Microsoft.Azure.Cosmos;
 using CosmosMassUpdate;
+using ItemDetail;
+using QueryWorkspace;
 
 public class Program
 {
@@ -19,83 +21,14 @@ public class Program
     static async Task Main()
     {
         //await BulkCreation.BulkTransactionCreation();
-        //await BulkItemPatch.BulkTransactionUpdate();
-        await BulkItemPatch.LoadItemsToUpdate();
+        await BulkItemPatch.BulkTransactionUpdate();
+        //await BulkItemPatch.LoadItemsToUpdate();
+        await ReadMany.QueryManyItemsAsync();
     }
 
-    private static async Task<List<itempoco>> LoadItemsToUpdate()
+    private static IReadOnlyCollection<Item_poco> GetItemsToInsert()
     {
-        CosmosClient cosmosClient = new CosmosClient(Program.EndpointUrl, Program.AuthorizationKey, new CosmosClientOptions() { AllowBulkExecution = true });
-        Database database = cosmosClient.GetDatabase(Program.DatabaseName);
-        Microsoft.Azure.Cosmos.Container container = database.GetContainer(Program.ContainerName);
-        var cmd = "select * from c where c.statuscode = \"3\"";
-        QueryDefinition query = new QueryDefinition(cmd);
-        var queryResultSetIterator = container.GetItemQueryIterator<itempoco>(query);
-
-        List<Program.itempoco> items_list = new List<Program.itempoco>();
-        try
-        {
-            while (queryResultSetIterator.HasMoreResults)
-            {
-                FeedResponse<itempoco> currentResultSet = await queryResultSetIterator.ReadNextAsync();
-                foreach (itempoco current in currentResultSet)
-                {
-                    items_list.Add(current);
-                }
-            }
-            Console.WriteLine("Number of Items : " + items_list.Count);
-            return items_list;
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message.ToString());
-            return null;
-        }
-    }
-
-    private static async Task<bool> BulkTransactionUpdate(List<itempoco> item_list)
-    {
-        await LoadItemsToUpdate();
-        try
-        {
-            var options = new CosmosClientOptions() { AllowBulkExecution = true };
-            var cosmosClient = new CosmosClient(Program.EndpointUrl, Program.AuthorizationKey, options);
-            var container = cosmosClient.GetContainer("bulk-tutorial", "Posts");
-            //posts.ForEach(t => t.Tags = "Update tags");
-            Stopwatch watch = Stopwatch.StartNew();
-            List<Task> concurrentTasks = new List<Task>();
-            foreach (itempoco itemtoUpdate in item_list)
-            {
-                //tasks.Add(container.CreateItemAsync(item, new PartitionKey(item.partitionKey)
-                var tsk = container.PatchItemAsync<itempoco>(
-                        id: itemtoUpdate.id,
-                        partitionKey: new PartitionKey(itemtoUpdate.partitionKey),
-                        patchOperations: new[] { PatchOperation.Replace("/statuscode", "4") });
-                concurrentTasks.Add(tsk);
-            }
-            await Task.WhenAll(concurrentTasks);
-            watch.Stop();
-            TimeSpan ts = watch.Elapsed;
-            Console.WriteLine($"The time it took to execute this bulk patch: {ts}");
-            return true;
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message.ToString());
-            return false;
-        }
-
-    }
-    public class itempoco
-    {
-        public string id { get; set; }
-        public string partitionKey { get; set; }
-        public string username { get; set; }
-
-    }
-    private static IReadOnlyCollection<itempoco> GetItemsToInsert()
-    {
-        return new Bogus.Faker<itempoco>()
+        return new Bogus.Faker<Item_poco>()
             .StrictMode(true)
             //Generate item
             .RuleFor(o => o.id, f => Guid.NewGuid().ToString()) //id
