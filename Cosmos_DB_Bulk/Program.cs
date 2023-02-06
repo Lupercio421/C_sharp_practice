0fbbd1fbb2d435d278a7195e9de430a25d32f223
Daniel L <daniel.llupercio@gmail.com>
2023-02-05 22:08:43 -0500

    new cosmos url and PK, commented the GetItemsToInsert Task

--- Cosmos_DB_Bulk/Program.cs
+++ Cosmos_DB_Bulk/Program.cs
@@ -11,29 +11,37 @@ using Microsoft.Azure.Cosmos;
 using CosmosMassUpdate;
 using ItemDetail;
 using QueryWorkspace;
+using Bogus.DataSets;
 
 public class Program
 {
+    public const string EndpointUrl = "https://05e7fe8c-0ee0-4-231-b9ee.documents.azure.com:443/";
+    public const string AuthorizationKey = "MzgVdSBTOVsffoRc6ovw81FEsAwhpZmKc0DgiLlkwqsS9bJyQpPguqnC46mmA5bzS3yyspy4Q995ACDblpoKXQ==";
     public const string DatabaseName = "bulk-tutorial";
     public const string ContainerName = "items";
-    public const int AmountToInsert = 10000;
+    public const int AmountToInsert = 20;
 
     static async Task Main()
     {
         //await BulkCreation.BulkTransactionCreation();
         await BulkItemPatch.BulkTransactionUpdate();
         //await BulkItemPatch.LoadItemsToUpdate();
-        await ReadMany.QueryManyItemsAsync();
+        //await ReadMany.QueryManyItemsAsync();
     }
 
-    private static IReadOnlyCollection<Item_poco> GetItemsToInsert()
-    {
-        return new Bogus.Faker<Item_poco>()
-            .StrictMode(true)
-            //Generate item
-            .RuleFor(o => o.id, f => Guid.NewGuid().ToString()) //id
-            .RuleFor(o => o.username, f => f.Internet.UserName())
-            .RuleFor(o => o.partitionKey, (f, o) => o.id) //partitionkey
-            .Generate(AmountToInsert);
-    }
+    //private static IReadOnlyCollection<Item_poco> GetItemsToInsert()
+    //{
+    //    return new Bogus.Faker<Item_poco>()
+    //        .StrictMode(true)
+    //        //Generate item
+    //        .RuleFor(o => o.id, f => Guid.NewGuid().ToString()) //id
+    //        .RuleFor(o => o.username, f => f.Internet.UserName())
+    //        .RuleFor(o => o.partitionKey, f => Guid.NewGuid().ToString()) //id
+    //        .RuleFor(o => o.vehicle_manufacturer, f => f.Vehicle.Manufacturer())
+    //        .RuleFor(o => o.vehicle_model, f => f.Vehicle.Model())
+    //        .RuleFor(o => o.company, f => f.Company.CompanyName())
+    //        .RuleFor(o => o.hashids, f => f.Hashids.ToString())
+    //        //.RuleFor(o => o.partitionKey, (f, o) => o.id) //partitionkey
+    //        .Generate(AmountToInsert);
+    //}
 }
