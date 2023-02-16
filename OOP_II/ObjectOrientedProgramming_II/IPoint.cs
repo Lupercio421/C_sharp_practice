e81eaf7b1dfa38671ad6635999d202ea34680fe8
Daniel L <daniel.llupercio@gmail.com>
2023-02-15 22:22:02 -0500

    learning about interfaces from Microsoft Documentation

---
+++ OOP_II/ObjectOrientedProgramming_II/IPoint.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/interface
+namespace ObjectOrientedProgramming_II
+{
+    internal interface IPoint
+    {
+        int X { get; set; }
+        int Y { get; set; }
+        double Distance { get; }
+    }
+    class Point : IPoint
+    {
+        //Constructor:
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        //Property implementation:
+        public int X { get; set; }
+        public int Y { get; set; }
+        //Property implementations
+        public double Distance =>
+            Math.Sqrt(X * X + Y * Y);
+    }
+    class MainClass
+    {
+        static void PrintPoint(IPoint p)
+        {
+            Console.WriteLine("x={0}, y = {1}", p.X, p.Y);
+        }
+        static void Main()
+        {
+            IPoint p = new Point(2,3);
+            Console.Write("My Point: ");
+            PrintPoint(p);
+        }
+    }
+}
---
+++ OOP_II/ObjectOrientedProgramming_II/ISampleInteface.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgramming_II
+{
+    internal interface ISampleInterface
+    {
+        void SampleMethod();
+    }
+    class ImplementationClass : ISampleInterface
+    {
+        void ISampleInterface.SampleMethod()
+        {
+            Console.WriteLine("How does this work?");
+        }
+        static void Main()
+        {
+            //Declare an interface instance.
+            ISampleInterface obj = new ImplementationClass();
+            //Call the member
+            obj.SampleMethod();
+        }
+    }
+}
