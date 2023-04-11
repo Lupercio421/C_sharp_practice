56edf996566a1a40b9c53920a631745c69506085
Daniel L <daniel.llupercio@gmail.com>
2023-04-10 22:58:39 -0400

    making the cosmos db cosmos client thread safe

---
+++ Cosmos_DB_Bulk/CosmosDBSingletonClient.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmos_DB_Bulk
+{
+    public class CosmosDBSingletonClient
+    {
+        private static object synclock = new object();
+        private static CosmosDBSingletonClient instance = new CosmosDBSingletonClient();
+        private readonly CosmosClient _client;
+
+        private CosmosDBSingletonClient()
+        {
+            var connectionString = "";
+            var clientOptions = new CosmosClientOptions()
+            {
+                AllowBulkExecution = true,
+            };
+            _client = new CosmosClient(connectionString, clientOptions);
+        }
+
+        public static CosmosDBSingletonClient Instance
+        {
+            get
+            {
+                lock (synclock)
+                {
+                    if (CosmosDBSingletonClient.instance == null)
+                    {
+                        CosmosDBSingletonClient.instance = new CosmosDBSingletonClient();
+                    }
+                    return CosmosDBSingletonClient.instance;
+                }
+            }
+        }
+        public CosmosClient Client
+        { 
+            get 
+            { 
+                return _client;
+            }
+        }
+    }
+}
