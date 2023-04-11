279fcb5c34643ef2207b8e411657679137e7f8ef
Daniel L <daniel.llupercio@gmail.com>
2023-04-10 22:59:00 -0400

    partial and sealed class/method lesson from OOP II

---
+++ OOP_II/ObjectOrientedProgramming_II/Partial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgramming_II
+{
+    internal class Partial
+    {
+        //The partial keyword specify that other parts of the class can be defined in the namespace.
+        //It is mandatory to use the partial keyword if we are trying to make a class partial
+        //All parts of a partial class should be in the same namespace.
+        //Things to remember about Partial Method
+        //partial keyword.
+        //return type void.
+        //implicitly private.
+        //and cannot be virtual.
+    }
+
+    internal class Sealed
+    {
+        //We use sealed classes to prevent inheritance. As we cannot inherit from a sealed class,
+        //the methods in the sealed class cannot be manipulated from other classes.
+
+        //One of the best uses of sealed classes is when you have a class with static members.
+
+    }
+}
