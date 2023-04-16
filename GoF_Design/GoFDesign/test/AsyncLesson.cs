046bfe5a12b3b35a6ea5f090570e97054f0ca3d1
Daniel L <daniel.llupercio@gmail.com>
2023-04-15 23:16:44 -0400

    async programming lessons

---
+++ GoF_Design/GoFDesign/test/AsyncLesson.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoFDesign.test
+{
+    public class AsyncLesson
+    {
+        public async Task<int> GetUrlContentLengthAsync()
+        {
+            var client = new HttpClient();
+
+            Task<string> getStringTask = client.GetStringAsync("https://learn.microsoft.com/dotnet");
+
+            DoIndependentWork();
+
+            string contents = await getStringTask;
+
+            return contents.Length;
+        }
+
+        void DoIndependentWork()
+        {
+            Console.WriteLine("Working");
+        }
+    }
+}
