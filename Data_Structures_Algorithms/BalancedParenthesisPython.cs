534240add2436b239898ca26f244cdb328452b00
Daniel L <daniel.llupercio@gmail.com>
2023-04-17 22:48:12 -0400

    Implementing the balanced symbols example fromm 3.3 of python txtbook

---
+++ Data_Structures_Algorithms/BalancedParenthesisPython.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_Algorithms
+{
+    public class BalancedParenthesisPython
+    {
+        public bool parChecker(string symbolString)
+        {
+            Stack<char> s = new Stack<char>();
+
+            var balanced = true;
+
+            int index = 0;
+
+            while (index < symbolString.Length && balanced) 
+            {
+                char symbol = symbolString[index];
+
+                if (symbol == '(' || symbol == '[' || symbol == '{')
+                {
+                    s.Push(symbol);
+                }
+                else
+                {
+                    if (s.Count == 0)
+                    {
+                        balanced = false;
+                    }
+                    else
+                    {
+                        char top = s.Pop();
+                        if (!Matches(top, symbol))
+                        {
+                            balanced = false;
+                        }
+                    }
+                }
+                index++;
+            }
+            if (balanced && s.Count == 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        private bool Matches(char open, char close)
+        {
+            string opens = "({[";
+            string closers = ")}}";
+
+            return opens.IndexOf(open) == closers.IndexOf(close);
+        }
+    }
+}
